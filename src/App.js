53412b08bfd841821144a836d1e3306e70505823
Babeth <elisabeth.jousselin@hotmail.fr>
2018-11-21 17:16:15 +0100

    modif des requetes

--- src/App.js
+++ src/App.js
@@ -1,47 +1,44 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-
+import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
   state = {
-    assoName :"",
-    assoLogo : "",
-  }
+    assoName: "",
+    assoLogo: ""
+  };
 
-  handleChangeTxt = (e) => {
-    this.setState({assoName: e.target.value});
-    
-  }
-  handleChangeLogo = (e) => {
-    this.setState({assoLogo: e.target.value});
-    
-  }
-  
-  submitAssoName = (e) => {
-    console.log('tutu',this.state);
+  handleChangeTxt = e => {
+    this.setState({ assoName: e.target.value });
+  };
+  handleChangeLogo = e => {
+    this.setState({ assoLogo: e.target.value });
+  };
+
+  submitAssoName = e => {
+    console.log("tutu", this.state);
     e.preventDefault();
-    if (!this.state.assoName) alert('asso name is empty');
+    if (!this.state.assoName) alert("asso name is empty");
     else {
-      axios.post('http://localhost:3002/asso', this.state)
-        .then(this.setState({assoName: '',assoLogo:''}))
-        // .then(window.location.reload());
-    }
-  }
-
-  modifyAsso = (id=2) => {
-    // const assoNameToModify = this.state.allAsso.filter(item => item.id === id)[0].AssoName;
-    const newName = prompt("What the new asso name ?", 'emmaus');
-    if (newName)
-    console.log('test',newName,id);
-    
-      axios.put(`http://localhost:3002/asso/${id}`,{ newName })
+      axios
+        .post("http://localhost:3002/asso", this.state)
+        .then(this.setState({ assoName: "", assoLogo: "" }));
       // .then(window.location.reload());
+    }
+  };
+
+  modifyAsso = (id = 2) => {
+    // const assoNameToModify = this.state.allAsso.filter(item => item.id === id)[0].AssoName;
+    const newName = prompt("What the new asso name ?", "emmaus");
+    if (newName) console.log("test", newName, id);
+
+    axios.put(`http://localhost:3002/asso/${id}`, { newName });
+    // .then(window.location.reload());
   };
 
   render() {
     return (
-      <div >
-         <form onSubmit={this.submitAssoName}>
+      <div>
+        <form onSubmit={this.submitAssoName}>
           <input
             type="text"
             name="assoName"
@@ -57,10 +54,10 @@ class App extends Component {
             onChange={this.handleChangeLogo}
           />
           <button type="submit">Submit</button>
-
-      </form>
-      <button type="" onClick={()=>this.modifyAsso()}>modifier</button>
-        
+        </form>
+        <button type="" onClick={() => this.modifyAsso()}>
+          modifier
+        </button>
       </div>
     );
   }
