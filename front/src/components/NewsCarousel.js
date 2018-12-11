6d47803355c064708e42b22fb8c751739e1c3da3
Hugo Trichard <hugo.trichard@gmail.com>
2018-12-11 15:55:33 +0100

    creation de la home, de la navbar et du carousel

---
+++ front/src/components/NewsCarousel.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import './NewsCarousel.css';
+
+class NewsCarousel extends React.Component {
+    render() {
+        return (
+            <Carousel autoPlay infiniteLoop={true} showThumbs={false} useKeyboardArrows={true}>
+                {this.props.news.map(e => {
+                    return ( <div className='item'>
+                                <img className='img' src={e.images} />
+                                <p style={{backgroundColor: 'transparent'}} className='legend'><span className='title'>{e.title} - {new Intl.DateTimeFormat().format(new Date(e.date))}</span><br/><br/><span className='text'>{e.text}</span></p>
+                            </div> )  
+                })}
+            </Carousel>
+        );
+    }
+};
+ 
+export default NewsCarousel;
