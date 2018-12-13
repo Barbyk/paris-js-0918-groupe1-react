4aecd761869ca10d42ec9b84dd299d4b6a6d5e4e
veensy <veensy@gmail.com>
2018-12-13 09:59:28 +0100

    map

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import './components/Map.css'
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Map from './components/Map'
 
 class App extends Component {
   render() {
@@ -12,6 +14,7 @@ class App extends Component {
         <Header />
         <Switch>
             <Route exact path='/' component={Home} />
+            <Route path="/trouverunemaraude" component ={ Map } />
         </Switch>
         <Footer />
       </div>
---
+++ front/src/components/DepartementId.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DepartementId extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
