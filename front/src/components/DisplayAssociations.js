91634ced80931a8e993f454a5c0208d25bc8b777
eddyte <eddy.te@gmail.com>
2018-12-19 16:13:09 +0100

    fix bug & add actions icon

--- front/src/components/DisplayAssociations.js
+++ front/src/components/DisplayAssociations.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import "./DisplayAssociations.css"
+import colis from "../assets/colis.png"
+import mauraudes from "../assets/mauraudes.png"
+import tables from "../assets/tables.png"
+
 
 export default class DisplayAssociations extends Component {
-  
+
+  // à faire: recuperer toutes les autres icones d'actions et les importer ici
+  tab = [
+    colis, mauraudes, tables
+  ]
+
   render() {
     return (
       <div className = "nocard">
@@ -10,7 +19,10 @@ export default class DisplayAssociations extends Component {
         <div><p>{this.props.logo}</p></div>
         <div><p>{this.props.address}</p></div>
         <div><p>{this.props.social_1}</p></div>
-        <div><p>{this.props.icon}</p></div>
+        <div><p>{this.props.icon ? this.props.icon.map((e)=>
+            <img src={this.tab[e-1]}
+            alt=""/>
+          ):null} </p></div>
         
         
       </div>
