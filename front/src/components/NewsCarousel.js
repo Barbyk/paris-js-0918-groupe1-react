6fec8a1fb8781ca881a581b543ba35a0245633fe
eddyte <eddy.te@gmail.com>
2019-01-07 16:46:28 +0100

    fix caroussel

--- front/src/components/NewsCarousel.js
+++ front/src/components/NewsCarousel.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "../../node_modules/react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import './NewsCarousel.css';
 import moment from 'moment'
@@ -12,10 +12,9 @@ class NewsCarousel extends React.Component {
                 {this.props.news.map(e => {
                     return ( <div className='item'>
                                 <img className='img' src={e.images} alt="" />
-                                <p style={{backgroundColor: 'transparent'}} className='legend'><span className='title'>{e.title} - {new Intl.DateTimeFormat().format(new Date(e.date))}</span><br/><br/><span className='text'>{e.text}</span></p>
-                                <img style={{height:"300px" }} className='img' alt="caroussel" src={e.img_url} />
+                                <p style={{backgroundColor: 'transparent'}} className='legend'><span className='title'>{e.title}</span><br/><br/><span className='text'>{e.text}</span></p>
+                                <img className='img' alt="caroussel" src={e.img_url} />
                                 <p style={{backgroundColor: 'transparent' }} className='legend'>
-                                <span className='title'>{e.title} </span><br/>
                                 <span className='date'>{moment(e.date).format('DD/MM/YYYY')}</span><br/>
                                 <span className='text'>{e.text}</span></p>
                                 
